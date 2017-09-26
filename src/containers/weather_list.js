3eabba7628be99ece17e9b83eb25be3c7719c4af
Andreas <AndreasPapaz@gmail.com>
2017-09-25 19:18:51 -0500

    css fix

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,9 +13,9 @@ class WeatherList extends Component {
 		return (
 			<tr key={name} >
 				<td>{name}</td>
-				<td><Chart data={temps} color='red' /></td>
-				<td><Chart data={pressures} color='green' /></td>
-				<td><Chart data={humidities} color='black' /></td>
+				<td><Chart data={temps} units='K' color='red' /></td>
+				<td><Chart data={pressures} units='hPa' color='green' /></td>
+				<td><Chart data={humidities} units='%' color='black' /></td>
 			</tr>
 		);
 	}
@@ -26,9 +26,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
